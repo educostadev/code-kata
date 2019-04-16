6ef9361c6fbede38e135b537b5f46170285f1c4a
eduardo costa <educostabra@gmail.com>
2019-04-16 09:12:29 -0300

    Uma versão inicial da solução do problema.

--- stock-maximum-profit/src/main/java/Stock.java
+++ stock-maximum-profit/src/main/java/Stock.java
@@ -1,3 +1,4 @@
+import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -6,12 +7,42 @@ public class Stock {
 
 	private List<Integer> stockPrices = new ArrayList<>();
 
+	private int maxProffit = 0;
+
 	public Stock(int... prices) {
 		Arrays.stream(prices).forEach(price -> stockPrices.add(price));
 	}
 
-	public int getMaxProffit(){
-		return 0;
+	public void calculate() {
+		int buyed = 0;
+		for (int i = 0; i < stockPrices.size()-1; i++) {
+			int currentPrice = stockPrices.get(i);
+			int nexPrice = stockPrices.get(i + 1);
+			if (currentPrice < nexPrice) {
+				//Ação irá subir
+				if (buyed == 0) { //se nao comprei ainda devo comprar
+					buyed = currentPrice;
+					print("Comprou por {0} na iteração {1}", currentPrice, i);
+				}
+			} else {
+				//Ação irá cair ou o preço se manterá
+				if (buyed > 0) { //se ja comprei devo vender para lucrar
+					int proffit = currentPrice - buyed;
+					maxProffit += proffit;
+					buyed = 0;
+					print("Vendi por {0} na iteração {1} e obtive lucro de {3} acumulando lucro de {4}",
+							currentPrice, i, proffit, maxProffit);
+				}
+			}
+		}
+	}
+
+	private void print(final String message, Object... values) {
+		System.out.println(MessageFormat.format(message, values));
+	}
+
+	public int getMaxProffit() {
+		return maxProffit;
 	}
 
 }
--- stock-maximum-profit/src/test/java/StockTest.java
+++ stock-maximum-profit/src/test/java/StockTest.java
@@ -1,3 +1,5 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 
 class StockTest {
@@ -6,6 +8,9 @@ class StockTest {
 	void randomValuesA() {
 		Stock stock = new Stock(45, 24, 35, 31, 40, 38, 11);
 
+		stock.calculate();
+
+		assertEquals(20, stock.getMaxProffit());
 
 	}
 
