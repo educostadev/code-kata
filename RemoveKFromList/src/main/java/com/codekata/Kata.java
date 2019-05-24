8025aff6c9dbfebee290c65220c546846fe51ba3
eduardo costa <educostabra@gmail.com>
2019-05-24 15:41:40 -0300

    Setup of tests

--- RemoveKFromList/src/main/java/com/codekata/Kata.java
+++ RemoveKFromList/src/main/java/com/codekata/Kata.java
@@ -2,4 +2,8 @@ package com.codekata;
 
 public class Kata {
 
+	static ListNode<Integer> removeKFromList(ListNode<Integer> l, int k) {
+		return l;
+	}
+
 }
--- RemoveKFromList/src/test/java/com/codekata/KataTest.java
+++ RemoveKFromList/src/test/java/com/codekata/KataTest.java
@@ -1,6 +1,9 @@
 package com.codekata;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static com.codekata.Kata.removeKFromList;
+import static com.codekata.ListNode.toArray;
+import static com.codekata.ListNode.toNode;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 import java.util.stream.Stream;
 
@@ -13,20 +16,28 @@ import org.junit.jupiter.params.provider.MethodSource;
  */
 class KataTest {
 
+	/**
+	 * input
+	 * expected
+	 * k
+	 */
 	private static Stream<Arguments> readInput() {
 		return Stream.of(
-				Arguments.of("abacabad", 'c'),
-				Arguments.of("a", '_'),
+				Arguments.of(new int[] { 3, 1, 2, 3, 4, 5 }, new int[] { 1, 2, 4, 5 }, 3),
+				Arguments.of(new int[] { 1, 2, 3, 4, 5, 6, 7 }, new int[] { 1, 2, 3, 4, 5, 6, 7 }, 10),
+				Arguments.of(new int[] { 1000, 1000 }, new int[] {}, 1000),
+				Arguments.of(new int[] {}, new int[] {}, -1000),
+				Arguments.of(new int[] { 123, 456, 789, 0 }, new int[] { 123, 456, 789 }, 0)
 		);
 	}
 
 	@ParameterizedTest
 	@MethodSource("readInput")
-	void myTest(String input, char expected) {
-		assertEquals(expected, perform(input));
+	void myTest(int[] input, int[] expected, int k) {
+		assertArrayEquals(expected, toArray(removeKFromList(toNode(input), k)));
 	}
 
-	private char perform(String s) {
-		return ' ';
+	private int[] perform(int[] input, int k) {
+		return input;
 	}
 }
