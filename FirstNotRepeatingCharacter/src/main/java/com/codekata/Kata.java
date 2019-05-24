b813ce9e6da238f01a16fa10d4cc4aec56737087
eduardo costa <educostabra@gmail.com>
2019-05-24 14:12:24 -0300

    Solution #2

--- FirstNotRepeatingCharacter/src/main/java/com/codekata/Kata.java
+++ FirstNotRepeatingCharacter/src/main/java/com/codekata/Kata.java
@@ -1,23 +1,13 @@
 package com.codekata;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-
 public class Kata {
 
 	static char firstNotRepeatingCharacter(String s) {
-		Set<Character> allLetters = new HashSet<>();
-		List<Character> uniqueLettersOrdered = new ArrayList<>();
-		for (Character ch : s.toCharArray()) {
-			if (allLetters.add(ch)){
-				uniqueLettersOrdered.add(ch); //isUnique
-			}else{
-				uniqueLettersOrdered.remove(ch); //isRepeated
-			}
-		}
-		return uniqueLettersOrdered.isEmpty() ? '_' : uniqueLettersOrdered.get(0);
+		for (char ch : s.toCharArray())
+			if (s.indexOf(ch) == s.lastIndexOf(ch))
+				return ch;
+
+		return '_';
 	}
 
 }
