81520977d86fdfe51bf2fd90a760c0dda8356f06
eduardo costa <educostabra@gmail.com>
2019-08-05 16:36:22 -0300

    Refactory - introduce a integer to hold gap and removed the list.

--- BinaryGap/src/main/java/com/codekata/Solution.java
+++ BinaryGap/src/main/java/com/codekata/Solution.java
@@ -1,8 +1,5 @@
 package com.codekata;
 
-import java.util.ArrayList;
-import java.util.List;
-
 class Solution {
 
 	static int solution(int N) {
@@ -10,30 +7,30 @@ class Solution {
 		//INPUT: A positive integer
 		//OUTPUT: The length of the longest binary gap. Zero if N does not contain
 
-		//Convert Integer N to Binary
-		//Count the length of binary gaps
-		//Get the index of the first 1
-		//While there is a next 1 after the index
-		//get the next index of 1 after the index
-		//calculate and store the length between the first and the next index
-		//Update the index to the next index value
-		//Return the longest offset stored
+		//Get the first index for 1
+		//initialize gap with 0
+		//While has a nextIndex for 1 after index
+		//get the nextIndex for 1 after index
+		//calculate the gap between nextIndex and index
+		//if currentGap is greader than gap so gap is equal to current gap
+		//index not is equal to next index
+		//return gap
 
-		List<Integer> offSets = new ArrayList<>();
 		String binary = Integer.toBinaryString(N);
+		int gap = 0;
 		int index = binary.indexOf('1');
 		while (nextIndexAfter(index, binary) > 0) {
 			int nextIndex = nextIndexAfter(index, binary);
-			offSets.add(nextIndex - (index + 1));
+			int currentGap = nextIndex - (index + 1);
+			if (currentGap > gap)
+				gap = currentGap;
 			index = nextIndex;
 		}
-		return offSets.stream().max(Integer::compareTo).orElse(0);
+		return gap;
 	}
 
 	private static int nextIndexAfter(final int index, final String binary) {
 		return binary.indexOf('1', index + 1);
 	}
-
-
 
 }
