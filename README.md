672f221459be8aa21e9a26b960eb962d10f494a6
Eduardo Costa <eduardo.costa@ciandt.com>
2023-06-25 23:42:15 -0300

    formatting

---
+++ README.md
@@ -0,0 +1,2 @@
+
+For create a new empty project, you can read [this file](./code-kata-archetype/README.md)
--- SortingClassics/src/main/java/com/codekata/BubbleSort.java
+++ SortingClassics/src/main/java/com/codekata/BubbleSort.java
@@ -5,23 +5,22 @@ public class BubbleSort implements Solution {
 
     @Override
     public int[] solve(int[] input) {
-        int sortedUntil = input.length-1;
-        boolean isSorted = false;
-        while(!isSorted){
-            isSorted = true;
-            for (int i=0; i<sortedUntil;i++){
-                var numA = input[i];
-                var numB = input[i+1];
-                if (numA > numB){
-                    input[i+1] = numA;
-                    input[i] = numB;
-                    isSorted = false;
+        int sortedUntil = input.length - 1;
+        boolean sorted = false;
+        while (!sorted) {
+            sorted = true;
+            for (int i = 0; i < sortedUntil; i++) {
+                var leftValue = input[i];
+                var rightValue = input[i + 1];
+                if (leftValue > rightValue) {
+                    input[i + 1] = leftValue;
+                    input[i] = rightValue;
+                    sorted = false;
                 }
             }
-            sortedUntil = sortedUntil-1;
+            sortedUntil--;
         }
-
         return input;
     }
-    
+
 }
